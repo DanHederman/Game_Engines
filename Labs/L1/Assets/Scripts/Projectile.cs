46600f799b1e86bb42edd8362490a9d6e5b3b667
DanHederman <c15410232@mydit.ie>
2018-09-23 18:42:35 +0100

    Tank Game
    This is how far I managed to get during the first lab session

---
+++ Labs/L1/Assets/Scripts/Projectile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public GameObject projectile;
+
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+
+    public float dmg = 10;
+    public float range = 40;
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+}
---
+++ Labs/L1/Assets/Scripts/TankMoves.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMoves : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    private float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("s"))
+        {
+            rb.transform.Translate(0, 0, speed - Time.deltaTime);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            rb.transform.Translate(0, 0, speed + Time.deltaTime);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rb.transform.Rotate(0, speed + Time.deltaTime * 25, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.transform.Rotate(0, speed - Time.deltaTime * 25, 0);
+        }
+    }
+}
